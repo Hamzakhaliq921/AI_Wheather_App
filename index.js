ee2779d5a1cf326ad39d9a3941841f4b71724ca0
Hamzakhaliq921 <abdulkhaliq920285@gmail.com>
2026-04-19 12:38:21 -0600

    updatecard with weather

--- index.js
+++ index.js
@@ -32,4 +32,52 @@ updateMap(data.location.lat, data.location.lon);
 
 }
  
+  getweather('karachi')
+getweather('islamabad')
+getweather('lahore')
+getweather('peshawar')
+getweather('sindh')
+document.getElementById("subbutton").addEventListener("click", function(e){
+  e.preventDefault();
+  let city = document.getElementById("submit").value;
+  getweather(city);
+  updatecard(city);
+});
+
+
+function updatecard(city) {
+  fetchweather(city)
+    .then(data => {
+//i will do this to use data in chatbotdata
+    currentWeatherData = data;
+
+      document.getElementById('h1').textContent = data.current.temp_c + " °C";
+      document.getElementById('location1').textContent = "Location: " + data.location.name;
+      document.getElementById('country1').textContent = "Country: " + data.location.country;
+      document.getElementById('condition1').textContent = "Condition: " + data.current.condition.text;
+      document.getElementById('feellike1').textContent = "Feel Like: " + data.current.feelslike_c + " °C";
+
+      document.getElementById('condition2').textContent = data.current.condition.text;
+      document.getElementById('Pressure').textContent = "Pressure: " + data.current.pressure_mb + " mb";
+      document.getElementById('Visibility').textContent = "Visibility: " + data.current.vis_km + " km";
+      document.getElementById('Cloud').textContent = "Cloud Cover: " + data.current.cloud + " %";
+      document.getElementById('dew').textContent = "Dew Point: " + data.current.dewpoint_c + " °C";
+
+      document.getElementById('Wind').textContent = data.current.wind_kph + " km/h";
+      document.getElementById('Gust').textContent = "Gust: " + data.current.gust_kph + " km/h";
+      document.getElementById('UV').textContent = "UV Index: " + data.current.uv;
+      document.getElementById('win').textContent ="UV Index: "+ data.current.wind_kph + " km/h";
+      document.getElementById('daynight').textContent = "Day/Night: " + (data.current.is_day ? "Day" : "Night");
+    })
+    .catch(error => console.error("Error:", error));
+}
+
+
+const chatToggle = document.getElementById("chatToggle");
+const chatBox = document.getElementById("chatBox");
+const closeChat = document.getElementById("closeChat");
+const sendMsg = document.getElementById("sendMsg");
+const chatInput = document.getElementById("chatInput");
+const chatBody = document.getElementById("chatBody");
+
 
